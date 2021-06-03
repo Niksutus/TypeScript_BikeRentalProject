c7bb1c8103fbb1b23b704c310220e160a90d7a04
Niksutus <niko.kananenfi@gmail.com>
2021-06-03 15:23:39 +0300

    added borrowBike and returnBike methods in the BikeDatabase class

--- src/classes/bikeDatabase.class.ts
+++ src/classes/bikeDatabase.class.ts
@@ -21,11 +21,37 @@ export class BikeDatabase {
     return this.borrowedBikes;
   }
 
-  borrowBike(bike: Bike): void {
-      for(let availableBike of this.availableBikes){
-          if(bike === availableBike){
-            
-          }
+  borrowBike(memebershipPaid: boolean, bike: Bike): void {
+    if ((memebershipPaid = true)) {
+      //the function checks if the memebership has been paid
+      for (let availableBike of this.availableBikes) {
+        if (bike === availableBike) {
+          let indexOfBike = this.availableBikes.indexOf(bike);
+          this.availableBikes.splice(indexOfBike, 1);
+          this.borrowedBikes.push(bike);
+
+          console.log(`You have successfully borrowed the bike ${bike.bikeId}`);
+        } else {
+          console.log("This bike is not available for borrowing");
+        }
       }
+    } else {
+      console.log("You don't currently have the right to borrow a bike")
+    }
+  }
+
+  returnBike(bike:Bike):void {
+    for(let available of this.availableBikes){
+      if(bike === available){
+        console.log("This bike has already been returned")
+      } else {
+
+        this.availableBikes.push(bike);
+        let indexOfBike = this.borrowedBikes.indexOf(bike);
+        this.borrowedBikes.splice(indexOfBike, 1)
+        console.log(`You have succesfully returned the bike ${bike.bikeId}`)
+
+      }
+    }
   }
 }
